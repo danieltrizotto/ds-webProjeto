1cc57f47846fc78b9790ff0b8679d0c3cbab9efc
danieltrizotto <danielbaltri@gmail.com>
2024-03-14 17:25:07 -0300

    responsidade e bd em andamento

---
+++ loja eletronica.sql
@@ -0,0 +1,17 @@
+use eletro_senai;
+
+create table LOGIN(
+idUsuario int AUTO_INCREMENT PRIMARY KEY,
+usuario varchar(100)  not null,
+senha varchar(100)  not null
+);
+create table CATEGORIAS(
+idCategoria int AUTO_INCREMENT PRIMARY KEY,
+categoria varchar(100) not null
+);
+create table PRODUTOS(
+idProdutos int AUTO_INCREMENT PRIMARY KEY,
+produto varchar(100) NOT NULL,
+categoria varchar(100) NOT NULL,
+FOREIGN KEY(categoria) references categorias(categoria)
+);
